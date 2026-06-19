ddddee3261979ac5d40fae207121d05dcdc42358
bielandrey <gabrielandrey3016@gmail.com>
2026-06-19 00:42:48 -0300

    feat: implementa funcoes Update e Delete no gerenciador

--- src/gerenciador.py
+++ src/gerenciador.py
@@ -1,21 +1,13 @@
+def atualizar_tarefa(id_tarefa, novo_status):
+    for tarefa in tarefas:
+        if tarefa["id"] == id_tarefa:
+            tarefa["status"] = novo_status
+            return tarefa
+    return None
 
-tarefas = []
-
-def adicionar_tarefa(id_tarefa, titulo, descricao):
-    """
-    Função Create: Adiciona uma nova tarefa ao sistema.
-    """
-    nova_tarefa = {
-        "id": id_tarefa,
-        "titulo": titulo,
-        "descricao": descricao,
-        "status": "A Fazer"
-    }
-    tarefas.append(nova_tarefa)
-    return nova_tarefa
-
-def listar_tarefas():
-    """
-    Função Read: Retorna todas as tarefas registradas.
-    """
-    return tarefas
+def remover_tarefa(id_tarefa):
+    for tarefa in tarefas:
+        if tarefa["id"] == id_tarefa:
+            tarefas.remove(tarefa)
+            return True
+    return False
